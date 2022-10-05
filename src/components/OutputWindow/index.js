9126ea508d4eb96c6936872f1dbd31cd86c2f18d
Alkarim Jiwa <ajiwa10@my.bcit.ca>
2022-10-04 17:45:40 -0700

    Reverting previous commit, automation testing done

--- src/components/OutputWindow/index.js
+++ src/components/OutputWindow/index.js
@@ -26,7 +26,7 @@ const OutputWindow = ({ outputDetails }) => {
 
     return (
         <>
-            <OutputTitle>Outputt</OutputTitle>
+            <OutputTitle>Output</OutputTitle>
             <OutputScreen>
                 {outputDetails ? getOutput() : null}
             </OutputScreen>
