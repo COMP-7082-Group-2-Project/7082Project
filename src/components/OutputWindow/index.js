44c8ee46d155833bee80386582afe8960c3bcdbd
orion13579 <71198530+orion13579@users.noreply.github.com>
2022-10-06 15:21:11 -0700

    Test case for GitHub Actions

--- src/components/OutputWindow/index.js
+++ src/components/OutputWindow/index.js
@@ -26,7 +26,7 @@ const OutputWindow = ({ outputDetails }) => {
 
     return (
         <>
-            <OutputTitle>Output</OutputTitle>
+            <OutputTitle>Output Window</OutputTitle>
             <OutputScreen>
                 {outputDetails ? getOutput() : null}
             </OutputScreen>
