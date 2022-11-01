acfb6e70722cb30dac1f5e479c2be0fdfc20f548
orion13579 <sloren@my.bcit.ca>
2022-11-01 12:13:19 -0700

    Added cleanup after each test

--- src/__tests__/testing.test.js
+++ src/__tests__/testing.test.js
@@ -1,10 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import App from '../App';
 
+afterEach(() => {
+    cleanup();
+})
+
 test('default test', () => {
     expect(true).toBe(true);
-})
+});
 
 test('renders "Compile and Execute" button', () => {
   render(<App />);
