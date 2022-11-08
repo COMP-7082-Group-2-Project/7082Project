a0021e3e37482d22469eec7a7371b0302f480e04
orion13579 <sloren@my.bcit.ca>
2022-11-08 14:38:11 -0800

    Started testing editor

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -7,12 +7,45 @@ describe('My First Test', () => {
   })
 })
 
-describe('Sample E2E Testing', () => {
+describe('Default Compile and Execute Test', () => {
   it('click on "Compile and Execute" button', () => {
     cy.visit('https://transcendent-tarsier-75164d.netlify.app/')
 
     const compile_button = cy.findByText(/compile and execute/i);
     compile_button.click();
   })
+
+  it('Check existance of output in window', () => {
+    const output_window = cy.findByText(/output/i).next();
+    output_window.should('have.text', 'ée');
+  })
 })
 
+// describe('Write Code, Compile and Execute, and See Output Test.', () => {
+//   it('Open Webpage', () => {
+//     cy.visit('https://transcendent-tarsier-75164d.netlify.app/')
+//   })
+
+//   it('Write Javascript Console Log ("Hello World") into Code Editor', () => {
+//     // Reference: https://stackoverflow.com/questions/56617522/testing-monaco-editor-with-cypress
+
+//     cy.get('#editor')
+//     .click()
+//     // change subject to currently focused element
+//     .focused()
+//     .type('{ctrl}a')
+//     .type('{rightArrow}')
+//     .type('console.log("Hello World!");')
+//     .type('{enter}')
+//   })
+  
+//   it('click on "Compile and Execute" button', () => {
+//     const compile_button = cy.findByText(/compile and execute/i);
+//     compile_button.click();
+//   })
+
+//   it('Check existance / correctness of output in window', () => {
+//     const output_window = cy.findByText(/output/i).next();
+//     output_window.should('have.text', 'Hello World!');
+//   })
+// })
