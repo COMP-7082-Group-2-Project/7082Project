a9ea80930b3fb7d9d7a6ebb70a403086e6d33d23
orion13579 <sloren@my.bcit.ca>
2022-11-08 17:09:57 -0800

    Added accordian test

---
+++ src/components/Accordion/Accordion.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { render, fireevent , screen } from "@testing-library/react";
+import Accordian from "./index.js";
+
+
+  const explanation = {"test1":"Hello", "test2":"There"};
+
+
+//on render
+test("accordian on render", () => {
+    render(<Accordian id={2} explanations={explanation}/>);
+    expect(screen.getByText(/Example Solutions/i));
+});
+
+// //Toggle
+test("accordian on toggle", () => {
+    render(<Accordian/>);
+    expect(screen.getByText(/Example Solutions/i));
+});
+
+// //becomes what it should be toggled to
+// test("accordian toggle expected"), () => {
+//     render(<Accordian />);
+
+
+// }
