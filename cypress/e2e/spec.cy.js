33bcfd0c0d1f2690c0bac156f39ff64cf4c64ac3
orion13579 <sloren@my.bcit.ca>
2022-11-12 16:00:44 -0800

    Added select different language test and uncommented rest of tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -20,7 +20,7 @@ describe('Write Code, Compile and Execute, and See Output Test.', () => {
   it('Write Javascript Console Log ("Hello World!") into Code Editor', () => {
     // Reference: https://stackoverflow.com/questions/56617522/testing-monaco-editor-with-cypress
 
-    cy.findByRole('code').get('.monaco-editor')
+    cy.findByRole('code')
     .click()
     // change subject to currently focused element
     .focused()
@@ -41,195 +41,213 @@ describe('Write Code, Compile and Execute, and See Output Test.', () => {
   })
 });
 
-// describe('Select Different Programming Language Test', () => {
-//   it('Open Webpage', () => {
-//     cy.visit('https://transcendent-tarsier-75164d.netlify.app/')
-//   })
+describe('Select Different Programming Language Test', () => {
+  it('Open Webpage', () => {
+    cy.visit('https://transcendent-tarsier-75164d.netlify.app/')
+  })
 
-//   it('click on Language Dropdown "Javascript" by Default', () => {
-//     const dropdown = cy.get('.css-fjipxn-control').contains('JavaScript');
-//     dropdown.click({force: true});
-//   })
+  it('click on Language Dropdown "Javascript" by Default', () => {
+    const dropdown = cy.get('.css-fjipxn-control').contains('JavaScript');
+    dropdown.click({force: true});
+  })
 
-//   it('click on Bash dropdown option', () => {
-//     const dropdown_option = cy.get("#react-select-2-listbox").contains('Bash');
-//     dropdown_option.trigger("onChange").click();
-//   })
+  it('click on C language dropdown option', () => {
+    const dropdown_option = cy.get("#react-select-2-listbox").contains('C (Clang 7.0.1)');
+    dropdown_option.trigger("onChange").click();
+  })
 
-//   it('test Bash langauge selected', () => {
-//     const dropdown = cy.get('.css-qc6sy-singleValue').contains('Bash');
-//     dropdown.should('be.visible');
-//   })
-// }); 
+  it('test C langauge selected', () => {
+    const dropdown = cy.get('.css-qc6sy-singleValue').contains('C (Clang 7.0.1)');
+    dropdown.should('be.visible');
+  })
 
-/*
-<span><span class="mtk1">console</span><span class="mtk15">.</span><span class="mtk1">log</span><span class="mtk15">(</span><span class="mtk14">"Hello&nbsp;World!"</span><span class="mtk15">);</span></span>
-*/
+  it('test correct operationablity by running program', () => {
+    cy.findByRole('code')
+    .click()
+    // change subject to currently focused element
+    .focused()
+    .type('{ctrl}a')
+    .type('{rightArrow}')
+    .type('int main() {')
+    .type('{enter}')
+    .type('printf("Hello World!");')
+    .type('{enter}')
+    .type('return 0;')
+    .type('{enter}')
 
-// describe('Choose Challenge Difficulty Test', () => {
-//   let previous_problem = "";
-//   it('Open Webpage', () => {
-//     cy.visit('https://transcendent-tarsier-75164d.netlify.app/');
-//   })
+    const compile_button = cy.findByText(/compile and execute/i);
+    compile_button.click();
 
-//   it('Test that text in code editor before selecting challenge problem to after selecting challenge problem canged', () => {
-//     cy.wait(loading_time);
-//     cy.get('.view-lines')
-//     // cy.get('.view-lines:nth-child(2)')
-//     .invoke('text')
-//     .then((text1) => {
-//       expect(text1).to.eq(welcome_text);
+    const output_window = cy.findByText(/output/i).next();
+    output_window.should('have.text', 'Hello World!');
+  })
+}); 
+
+describe('Choose Challenge Difficulty Test', () => {
+  let previous_problem = "";
+  it('Open Webpage', () => {
+    cy.visit('https://transcendent-tarsier-75164d.netlify.app/');
+  })
+
+  it('Test that text in code editor before selecting challenge problem to after selecting challenge problem canged', () => {
+    cy.wait(loading_time);
+    cy.get('.view-lines')
+    // cy.get('.view-lines:nth-child(2)')
+    .invoke('text')
+    .then((text1) => {
+      expect(text1).to.eq(welcome_text);
       
-//       // Click on dropdown
-//       const dropdown = cy.get('#react-select-4-placeholder').contains('Select Difficulty');
-//       dropdown.click({force: true});
+      // Click on dropdown
+      const dropdown = cy.get('#react-select-4-placeholder').contains('Select Difficulty');
+      dropdown.click({force: true});
       
-//       // Click on easy dropdown option
-//       const dropdown_option = cy.get("#react-select-4-listbox").contains('Easy');
-//       dropdown_option.trigger("onChange").click();
+      // Click on easy dropdown option
+      const dropdown_option = cy.get("#react-select-4-listbox").contains('Easy');
+      dropdown_option.trigger("onChange").click();
 
-//       // Compare problem text to welcome text
-//       cy.get('.view-lines')
-//       .invoke('text')
-//       .should((text2) => {
-//         expect(text1).not.to.eq(text2)
-//       });
-//     })
-//   })
-// });
+      // Compare problem text to welcome text
+      cy.get('.view-lines')
+      .invoke('text')
+      .should((text2) => {
+        expect(text1).not.to.eq(text2)
+      });
+    })
+  })
+});
 
-// describe('Change UI Theme Test', () => {
-//   it('Open Webpage', () => {
-//     cy.visit('https://transcendent-tarsier-75164d.netlify.app/');
+describe('Change UI Theme Test', () => {
+  it('Open Webpage', () => {
+    cy.visit('https://transcendent-tarsier-75164d.netlify.app/');
     
-//   })
+  })
   
-//   it('Test Default Theme "Oceanic Next"', () => {
-//     cy.findByRole('code').get('.monaco-editor').then($el => {
-//       const win = cy.state('window')
-//       const styles = win.getComputedStyle($el[0])
-//       const transform = styles.getPropertyValue('transform')
-//       console.log(transform)
-//       // expect(transform).to.eq(...)
-//     })
+  // it('Test Default Theme "Oceanic Next"', () => {
+  //   cy.findByRole('code').get('.monaco-editor').then($el => {
+  //     const win = cy.state('window')
+  //     const styles = win.getComputedStyle($el[0])
+  //     const transform = styles.getPropertyValue('transform')
+  //     console.log(transform)
+  //     // expect(transform).to.eq(...)
+  //   })
 
-//     cy.wait(loading_time);
-//     //.should('have.css', '--vscode-editor-background', '#1b2b34')
-//   })
+  //   cy.wait(loading_time);
+  //   //.should('have.css', '--vscode-editor-background', '#1b2b34')
+  // })
 
-//   it('click on Theme Dropdown', () => {
-//     const dropdown = cy.get('.css-fjipxn-control').contains('Oceanic');;
-//     dropdown.click({force: true});
-//   })
+  it('click on Theme Dropdown', () => {
+    const dropdown = cy.get('.css-fjipxn-control').contains('Oceanic');;
+    dropdown.click({force: true});
+  })
 
-//   it('click on "Blackboard" dropdown option', () => {
-//     const dropdown_option = cy.get("#react-select-3-listbox").contains('Hallows');
-//     dropdown_option.trigger("onChange").click();
-//   })
+  it('click on "Blackboard" dropdown option', () => {
+    const dropdown_option = cy.get("#react-select-3-listbox").contains('Blackboard');
+    dropdown_option.trigger("onChange").click();
+  })
 
-//   it('test Blackboard theme selected', () => {
-//     const dropdown = cy.get('.css-qc6sy-singleValue').contains('Blackboard');
-//     dropdown.should('be.visible');
-//   })
+  it('test Blackboard theme selected', () => {
+    cy.wait(loading_time);
+    const dropdown = cy.get('.css-qc6sy-singleValue').contains('Blackboard');
+    dropdown.should('be.visible');
+  })
 
-//   it('test Blackboard theme applied', () => {
-//     cy.wait(loading_time);
-//     cy.findByRole('code').get('.monaco-editor').then($el => {
-//       const win = cy.state('window')
-//       const styles = win.getComputedStyle($el[0])
-//       const transform = styles.getPropertyValue('transform')
-//       console.log(transform)
-//     })
-//     // --vscode-editor-background: #1b2b34
-//     //--vscode-editor-background: #0c1021
-//   })
-// });
-
-// describe('Skip Challenge Problem Test', () => {
-//   it('Open Webpage', () => {
-//     cy.visit('https://transcendent-tarsier-75164d.netlify.app/');
-//   })
-
-//   it('switch to an easy challenge problem', () => {
-//     cy.wait(loading_time);
-//     cy.get('.view-lines')
-//     .invoke('text')
-//     .then((text1) => {
-//       expect(text1).to.eq(welcome_text);
-      
-//       // Click on dropdown
-//       const dropdown = cy.get('#react-select-4-placeholder').contains('Select Difficulty');
-//       dropdown.click({force: true});
-      
-//       // Click on easy dropdown option
-//       const dropdown_option = cy.get("#react-select-4-listbox").contains('Easy');
-//       dropdown_option.trigger("onChange").click();
-
-//       // Compare problem text to welcome text
-//       cy.get('.view-lines')
-//       .invoke('text')
-//       .should((text2) => {
-//         expect(text1).not.to.eq(text2)
-//       });
-//     })
-//   })
-
-//   it('test that skip button shows a new question', () => {
-//     let skip_svg_class = "sc-cabOPr";
+  // it('test Blackboard theme applied', () => {
     
-//     cy.get('.view-lines')
-//     .invoke('text')
-//     .then((text1) => {
+  //   cy.findByRole('code').get('.monaco-editor').then($el => {
+  //     const win = cy.state('window')
+  //     const styles = win.getComputedStyle($el[0])
+  //     const transform = styles.getPropertyValue('transform')
+  //     console.log(transform)
+  //   })
+  //   --vscode-editor-background: #1b2b34
+  //   --vscode-editor-background: #0c1021
+  // })
+});
+
+describe('Skip Challenge Problem Test', () => {
+  it('Open Webpage', () => {
+    cy.visit('https://transcendent-tarsier-75164d.netlify.app/');
+  })
+
+  it('switch to an easy challenge problem', () => {
+    cy.wait(loading_time);
+    cy.get('.view-lines')
+    .invoke('text')
+    .then((text1) => {
+      expect(text1).to.eq(welcome_text);
       
-//       // Click skip button
-//       cy.get("svg." + skip_svg_class).click({force: true});
-//       cy.wait(loading_time);
-
-//       // Compare new problem text to old problem text and assert that it is different
-//       cy.get('.view-lines')
-//       .invoke('text')
-//       .should((text2) => {
-//         expect(text1).not.to.eq(text2);
-//       });
-//     })
-//   })
-// });
-
-// describe('Get Challenge Hints Test', () => {
-//   it('Open Webpage', () => {
-//     cy.visit('https://transcendent-tarsier-75164d.netlify.app/');
-//   })
-
-//   it('switch to an easy challenge problem', () => {
-//     cy.wait(loading_time);
-//     cy.get('.view-lines')
-//     .invoke('text')
-//     .then((text1) => {
-//       expect(text1).to.eq(welcome_text);
+      // Click on dropdown
+      const dropdown = cy.get('#react-select-4-placeholder').contains('Select Difficulty');
+      dropdown.click({force: true});
       
-//       // Click on dropdown
-//       const dropdown = cy.get('#react-select-4-placeholder').contains('Select Difficulty');
-//       dropdown.click({force: true});
+      // Click on easy dropdown option
+      const dropdown_option = cy.get("#react-select-4-listbox").contains('Easy');
+      dropdown_option.trigger("onChange").click();
+
+      // Compare problem text to welcome text
+      cy.get('.view-lines')
+      .invoke('text')
+      .should((text2) => {
+        expect(text1).not.to.eq(text2)
+      });
+    })
+  })
+
+  it('test that skip button shows a new question', () => {
+    let skip_svg_class = "sc-cabOPr";
+    
+    cy.get('.view-lines')
+    .invoke('text')
+    .then((text1) => {
       
-//       // Click on easy dropdown option
-//       const dropdown_option = cy.get("#react-select-4-listbox").contains('Easy');
-//       dropdown_option.trigger("onChange").click();
+      // Click skip button
+      cy.get("svg." + skip_svg_class).click({force: true});
+      cy.wait(loading_time);
 
-//       // Compare problem text to welcome text
-//       cy.get('.view-lines')
-//       .invoke('text')
-//       .should((text2) => {
-//         expect(text1).not.to.eq(text2)
-//       });
-//     })
-//   })
+      // Compare new problem text to old problem text and assert that it is different
+      cy.get('.view-lines')
+      .invoke('text')
+      .should((text2) => {
+        expect(text1).not.to.eq(text2);
+      });
+    })
+  })
+});
 
-//   it('test that hints show example solutions', () => {
-//     let hints_svg_class = "sc-iTFTee";
-//     let example_solution_tab_class = "sc-ipEyDJ";
+describe('Get Challenge Hints Test', () => {
+  it('Open Webpage', () => {
+    cy.visit('https://transcendent-tarsier-75164d.netlify.app/');
+  })
 
-//     cy.get("svg." + hints_svg_class).click();
+  it('switch to an easy challenge problem', () => {
+    cy.wait(loading_time);
+    cy.get('.view-lines')
+    .invoke('text')
+    .then((text1) => {
+      expect(text1).to.eq(welcome_text);
+      
+      // Click on dropdown
+      const dropdown = cy.get('#react-select-4-placeholder').contains('Select Difficulty');
+      dropdown.click({force: true});
+      
+      // Click on easy dropdown option
+      const dropdown_option = cy.get("#react-select-4-listbox").contains('Easy');
+      dropdown_option.trigger("onChange").click();
 
-//     cy.findByRole('dialog').findByText(/Example Solutions/i).next().should('have.class', example_solution_tab_class);
-//   })
-// });
+      // Compare problem text to welcome text
+      cy.get('.view-lines')
+      .invoke('text')
+      .should((text2) => {
+        expect(text1).not.to.eq(text2)
+      });
+    })
+  })
+
+  it('test that hints show example solutions', () => {
+    let hints_svg_class = "sc-iTFTee";
+    let example_solution_tab_class = "sc-ipEyDJ";
+
+    cy.get("svg." + hints_svg_class).click();
+
+    cy.findByRole('dialog').findByText(/Example Solutions/i).next().should('have.class', example_solution_tab_class);
+  })
+});
