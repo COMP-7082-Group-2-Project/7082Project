0ae5bb0153e5551d10dd68d42b131d43c70ee9a0
orion13579 <sloren@my.bcit.ca>
2022-11-30 19:43:22 -0800

    Updated api call

--- src/assets/Axios/AxiosInstance.js
+++ src/assets/Axios/AxiosInstance.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
 const AxiosInstance = axios.create({
-    baseURL: "https://judge0-ce.p.rapidapi.com",
-    // baseURL: "http://edicode.westus3.cloudapp.azure.com:2358",
+    // baseURL: "https://judge0-ce.p.rapidapi.com",
+    baseURL: "http://edicode.westus3.cloudapp.azure.com:2358",
     params: { base64_encoded: "true", fields: "*" },
-    headers: {
-        "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
-        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY
-    }
+    // headers: {
+    //     "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+    //     "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY
+    // }
 })
 
 AxiosInstance.defaults.headers.post['content-Type'] = 'application/json';
